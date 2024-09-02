9f2cbc97b53a6d8471d37a37a13dcf834dc2e258
virus <2017646+virusbb001@users.noreply.github.com>
2024-09-03 00:50:26 +0900

    complete step 5: mul, div, parentheses

--- 9cc.c
+++ 9cc.c
@@ -120,7 +120,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -213,36 +213,58 @@ Node *primary() {
   return new_node_num(expect_number());
 }
 
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("    push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("    pop rdi\n");
+  printf("    pop rax\n");
+  switch(node->kind) {
+    case ND_ADD:
+      printf("    add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("    sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("    imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("    cqo\n");
+      printf("    idiv rdi\n");
+      break;
+  }
+
+  printf("    push rax\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "引数の個数が正しくありません\n");
+    error("引数の個数が正しくありません");
     return 1;
   }
 
+  // トークナイズしてパースする
   user_input = argv[1];
-
-  // トークナイズする
   token = tokenize(argv[1]);
-  // print_tokens(token);
+  Node *node = expr();
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  // 式の最初は数でなければならないので、それをチェックして
-  // 最初のmov命令を出力
-  printf("    mov rax, %d\n", expect_number());
+  // 抽象構文木を下りながらコード生成
+  gen(node);
 
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("    add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("    sub rax, %d\n", expect_number());
-  }
+  // スタックトップに式全体の値が残っているはずなので
+  // それをRAXにロードして関数からの返り値とする
+  printf("    pop rax\n");
   printf("    ret\n");
   return 0;
 }
